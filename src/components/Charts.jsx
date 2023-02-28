0d8776478b9accf4bdcd66c14c8ccf5873687850
localhostd3veloper <gautamanand022@gmail.com>
2023-02-28 17:30:42 +0530

    chart data

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import * as XLSX from "xlsx";
 
 function Charts() {
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = XLSX.utils.sheet_to_json(worksheet);
+        console.log(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  };
+
   return (
-    <div>Charts</div>
-  )
+    <form>
+      <label htmlFor="upload">Upload File</label>
+      <input type="file" name="upload" id="upload" onChange={readUploadFile} />
+    </form>
+  );
 }
 
-export default Charts
+export default Charts;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center font-medium text-3xl">
-        Hello! <p className="text-lg"> Please select a task to continue.</p>
-      <div className="flex justify-between gap-10 text-lg">
-        <Link to='/forms' className="text-blue-500 underline">Task 1: Forms</Link>
-        <Link to='/charts' className="text-blue-500 underline">Task 2: ECG Data Chart</Link>
-      </div>
+      Hello! <p className="text-lg"> Please select a task to continue.</p>
+      <section className="flex flex-col sm:flex-row gap-3 justify-between md:gap-10 text-lg">
+        <Link to="/forms" className="text-blue-500 underline">
+          Task 1: Forms
+        </Link>
+        <Link to="/charts" className="text-blue-500 underline">
+          Task 2: ECG Data Chart
+        </Link>
+      </section>
     </div>
   );
 }
