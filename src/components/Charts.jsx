07c72840e90ac8e106f1b61695f52440ad2a5030
localhostd3veloper <gautamanand022@gmail.com>
2023-02-28 18:14:47 +0530

    ecg data

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,7 +1,44 @@
 import React, { useEffect, useState } from "react";
 import * as XLSX from "xlsx";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function Charts() {
+  const [mainData, setMainData] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [begin, setBegin] = useState(0);
+  const [end, setEnd] = useState(5);
+
+  const generateChartData = () => {
+    let data = [];
+    mainData.forEach((item, index) => {
+      if (index >= begin && index <= end) {
+        data.push(item);
+      }
+    });
+    setBegin(begin + 1);
+    setEnd(end + 1);
+    setChartData(data);
+  };
+  useEffect(() => {
+    generateChartData();
+  }, [mainData]);
+
+  useEffect(() => {
+  if(mainData){
+    setTimeout(() => {
+      generateChartData();
+    }, 1000);
+  }
+  });
+
   const readUploadFile = (e) => {
     e.preventDefault();
     if (e.target.files) {
@@ -12,17 +49,39 @@ function Charts() {
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const json = XLSX.utils.sheet_to_json(worksheet);
-        console.log(json);
+        setMainData(json);
       };
       reader.readAsArrayBuffer(e.target.files[0]);
     }
   };
 
   return (
-    <form>
-      <label htmlFor="upload">Upload File</label>
+    <div className="h-screen flex flex-col justify-center items-center"> 
       <input type="file" name="upload" id="upload" onChange={readUploadFile} />
-    </form>
+      <LineChart
+        width={1000}
+        height={300}
+        data={chartData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+        <YAxis dataKey="ECG" />
+        <Tooltip />
+        <Legend/>
+        <Line
+          type="monotone"
+          dataKey="ECG"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </div>
   );
 }
 
